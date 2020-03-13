f7ef9fd3d1783f191381e248fe8dd7c98fbc5e69
chosky <davidhenaoo98@gmail.com>
2020-03-12 19:47:43 -0500

    Se hacen los primero metodos de conexión con el fronend

--- project/geolocalizado/geolocalizado.py
+++ project/geolocalizado/geolocalizado.py
@@ -3,14 +3,34 @@ from flask import request
 from flask import Blueprint
 import json
 
+from project import mongo
+
 geolocalizador_app = Blueprint("geolocalizador_app", __name__)
 
 # Este metodo es solo una muestra de como se comienza a usar Flask y sus
 # peticiones pero este metodo no pertenece a backend
-@geolocalizador_app.route('/api/geolocalizado/activar_ubicacion', methods=['GET'])
-def activar_ubicacion():
+@geolocalizador_app.route('/api/geolocalizado/test_get', methods=['GET'])
+def test_get():
     try:
+        user = request.args['user']
+        edad = request.args['edad']
+        print(user)
+        return jsonify({message: 'pase'})
+    except Exception as excepciond:
+        print("No se logro hacer metodo get")
+        print(exception)
+        return jsonify({message: 'no pase'})
+
+    
+@geolocalizador_app.route('/api/geolocalizado/test_post')
+def test_post():
+    try:
+        correo = request.json['correo']
+        llave = request.json['llave']
+        print(correo)
         return jsonify(True)
-    except Exception as excepcion:
-        print("No se logro verificar si la ubicacion esta activada")
+    except Exception as exception:
+        print("NO se logro hacer metodo post")
+        print(exception)
         return jsonify(False)
+    
